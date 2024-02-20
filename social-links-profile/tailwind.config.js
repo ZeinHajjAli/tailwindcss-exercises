6e4fdbd7fd8b87fb18f3c20d6d9ce703a5b9c7aa
Zein Hajj-Ali <zein@lemay.ai>
2024-02-20 01:39:14 -0500

    Added social-links-profile project

---
+++ social-links-profile/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    fontFamily: {
+        'sans': ['Inter', 'sans-serif']
+    },
+    colors: {
+        green: 'hsl(75, 94%, 57%)',
+        white: 'hsl(0, 0%, 100%)',
+        grey: 'hsl(0, 0%, 20%)',
+        darkGrey: 'hsl(0, 0%, 12%)',
+        offBlack: 'hsl(0, 0%, 8%)',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
